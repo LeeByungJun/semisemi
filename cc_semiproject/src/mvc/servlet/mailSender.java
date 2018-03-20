22845f9486559d73bc9ae4345a54df0c9c6b90b0
LeeByungJun <semicolon92@naver.com>
2018-03-20 16:09:04 +0900

    ...

--- cc_semiproject/src/mvc/servlet/mailSender.java
+++ cc_semiproject/src/mvc/servlet/mailSender.java
@@ -32,57 +32,7 @@ public class mailSender extends HttpServlet {
 	 */
 	public mailSender() {
 		super();
-		// TODO Auto-generated constructor stub
-		// naver일 경우 smtp.naver.com
-		// google일 경우 smtp.gmail.com
-		/*
-		 * String host = "smtp.naver.com";
-		 * 
-		 * final String username = "pjlee92"; //네이버 아이디 입력 final String password =
-		 * "3apdlvmf3!"; //네이버 비밀번호 입력 int port = 465;
-		 * 
-		 * //메일 내용 String recipient = "semicolon92@naver.com"; //받는 사람의 메일 주소를 입력하세요.
-		 * String subject = "이메일 인증"; //메일 제목 String body = "1234"; //메일 내용
-		 * 
-		 * Properties props = System.getProperties(); //정보를 담기위한 객체 생성
-		 * 
-		 * //SMTP 서버 정보 설정 props.put("mail.smtp.host", host);
-		 * props.put("mail.smtp.port", port); props.put("mail.smtp.auth", "true");
-		 * props.put("mail.smtp.ssl.enable", "true"); props.put("mail.smtp.ssl.trust",
-		 * host);
-		 * 
-		 * //Session 생성 Session session = Session.getDefaultInstance(props,new
-		 * javax.mail.Authenticator() { String un = username; String pw = password;
-		 * protected javax.mail.PasswordAuthentication getPasswordAuthentication(){
-		 * return new javax.mail.PasswordAuthentication(un,pw); } });
-		 * session.setDebug(true); //for debug
-		 * 
-		 * Message mimeMessage = new MimeMessage(session); //MimeMessage 생성 try {
-		 * mimeMessage.setFrom(new InternetAddress("pjlee92@naver.com")); //발신자 세팅, 보내는
-		 * 사람의 이메일 주소 한번더 입력 mimeMessage.setRecipient(Message.RecipientType.TO, new
-		 * InternetAddress(recipient)); //수신자 셋팅. .to외에 .cc(참조), .bcc(숨은참조)도 있음
-		 * mimeMessage.setSubject(subject); //제목셋팅 mimeMessage.setText(body); //내용셋팅
-		 * Transport.send(mimeMessage); //javax.mail.Transport.send()이용 } catch
-		 * (AddressException e) { // TODO Auto-generated catch block
-		 * e.printStackTrace(); } catch (MessagingException e) { // TODO Auto-generated
-		 * catch block e.printStackTrace(); }
-		 */
 	}
-
-	/*
-	 * //naver일 경우 smtp.naver.com //google일 경우 smtp.gmail.com String host =
-	 * "smtp.naver.com";
-	 * 
-	 * final String username = "pjlee92"; //네이버 아이디 입력 final String password =
-	 * "3apdlvmf3!"; //네이버 비밀번호 입력 int port = 465;
-	 * 
-	 * //메일 내용 String recipient = "semicolon92@naver.com"; //받는 사람의 메일 주소를 입력하세요.
-	 * String subject = "이메일 인증"; //메일 제목 String body = "1234"; //메일 내용
-	 * 
-	 * Properties props = System.getProperties(); //정보를 담기위한 객체 생성
-	 * 
-	 * //SMTP 서버 정보 설정 Properties props1 = System.getProperties();
-	 */
 
 	/**
 	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse
